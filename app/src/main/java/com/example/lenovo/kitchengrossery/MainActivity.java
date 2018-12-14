a7ffa87b5174ee8cae25f52d1bd50683855aa311
harishpadmanabh-HP <harishpadmanabh.sics@gmail.com>
2018-12-15 00:24:04 +0530

    view pager added

--- app/src/main/java/com/example/lenovo/kitchengrossery/MainActivity.java
+++ app/src/main/java/com/example/lenovo/kitchengrossery/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lenovo.kitchengrossery;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,9 +14,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recy_srp;
+
+    ViewPager viewPager;
+    TabLayout indicator;
+    private List<Integer> imageites;
+
 
     int [] pic={R.drawable.chilli,R.drawable.ic_launcher_foreground,R.drawable.chilli,R.drawable.chilli};
 
@@ -40,6 +50,35 @@ public class MainActivity extends AppCompatActivity {
 
         recy_srp.setAdapter(vadapt);
         recy_srp.setLayoutManager(lm);
+
+        viewPager=(ViewPager)findViewById(R.id.viewPager);
+        indicator=(TabLayout)findViewById(R.id.indicator);
+        imageites=new ArrayList<>();
+        imageites.add(R.mipmap.image1);
+        imageites.add(R.mipmap.image11);
+        imageites.add(R.mipmap.image3);
+        imageites.add(R.mipmap.image4);
+        viewPager.setAdapter(new SliderAdapter(this,imageites));
+        indicator.setupWithViewPager(viewPager,true);
+        Timer timer=new Timer();
+        timer.scheduleAtFixedRate(new SliderTimer(),3000,3000);
+
+    }
+    class SliderTimer extends TimerTask {
+
+        @Override
+        public void run() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (viewPager.getCurrentItem()<imageites.size()-1){
+                        viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
+                    }else {
+                        viewPager.setCurrentItem(0);
+                    }
+                }
+            });
+        }
     }
 
     class Vertadapt extends RecyclerView.Adapter<Vertadapt.vh>
